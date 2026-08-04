6cfee2d48a9ac08051caf5246a7b5b0edc115e4b
ppruvost <patrick.pruvost50@gmail.com>
2026-08-04 16:45:51 +0200

    Remove math version print statement
    Removed version printing for the math module.

--- pyodide/app.js
+++ pyodide/app.js
@@ -237,7 +237,7 @@ matplotlib.use("Agg")
 print("Python", sys.version.split()[0])
 print("NumPy", numpy.__version__)
 print("Matplotlib", matplotlib.__version__)
-print("Math", math.__version__)
+print("Math")
 `);
 
                 pyodideReady = instance;
