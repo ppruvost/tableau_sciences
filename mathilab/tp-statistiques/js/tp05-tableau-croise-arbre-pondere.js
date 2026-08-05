47d4e8f968a1b3c86bbc9ab977865f7746f4b622
ppruvost <patrick.pruvost50@gmail.com>
2026-08-05 15:29:48 +0200

    Initialize tabs and context in tp05-tableau-croise-arbre-pondere

--- mathilab/tp-statistiques/js/tp05-tableau-croise-arbre-pondere.js
+++ mathilab/tp-statistiques/js/tp05-tableau-croise-arbre-pondere.js
@@ -95,6 +95,7 @@ function gererNiveauAffiche() {
 
 document.getElementById('select-filiere-pro')?.addEventListener('change', gererNiveauAffiche);
 
+initOngletsParFiliere();
 initContextePro({ filieres: FILIERES_PRO, contextes: CONTEXTES_S5 });
 initTableauCroise();
 initArbrePondere();
