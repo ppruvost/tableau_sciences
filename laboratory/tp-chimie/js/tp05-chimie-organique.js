4d65808dbc14fae79a68bfb895279ef5f57827d0
ppruvost <patrick.pruvost50@gmail.com>
2026-08-26 18:40:59 +0200

    Remove material selection from report generation
    Removed unused material selection logic from report generation.

--- laboratory/tp-chimie/js/tp05-chimie-organique.js
+++ laboratory/tp-chimie/js/tp05-chimie-organique.js
@@ -25,8 +25,7 @@ import {
 } from "../../js/securite.js";
 
 import {
-    initMateriel,
-    getMaterielSelectionne
+    initMateriel
 } from "../../js/materiel.js";
 
 import {
@@ -544,14 +543,6 @@ function lancerCompteRendu() {
         sections.push({
             titre: "Résumé du TP",
             texte: resume
-        });
-    }
-
-    const materiel = getMaterielSelectionne();
-    if (materiel.length) {
-        sections.push({
-            titre: "Matériel utilisé",
-            texte: materiel.join(" • ")
         });
     }
 
